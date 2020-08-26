dd1f3a98e233446e58ecb63dc39ea262f88c3fdd
morganjwilson <64968479+morganjwilson@users.noreply.github.com>
2020-08-26 14:56:34 -0400

    Complete

--- src/index.js
+++ src/index.js
@@ -1,32 +1,39 @@
-
 let todos = [
-    {
-        completed: false,
-        description: "Take Jim to the hair salon"
-    },
-    {
-        completed: true,
-        description: "Drop off wedding invitation at mailbox"
-    },
-    {
-        completed: false,
-        description: "Pick up the cake"
-    },
-    {
-        completed: false,
-        description: "Call the caterers"
-    }
+  {
+    completed: false,
+    description: "Take Jim to the hair salon",
+  },
+  {
+    completed: true,
+    description: "Drop off wedding invitation at mailbox",
+  },
+  {
+    completed: false,
+    description: "Pick up the cake",
+  },
+  {
+    completed: false,
+    description: "Call the caterers",
+  },
 ];
 
 function renderTodoApp() {
-    let app = document.querySelector("#app")
-    let h1 = document.createElement("h1")
-    // your code here
+  let app = document.querySelector("#app");
+  let h1 = document.createElement("h1");
+  let list = document.createElement("ul");
 
-    h1.innerText = "Todo List"
+  let html = "";
+  for (let todoItem of todos) {
+    html += `
+      <li>
+        completed: ${todoItem.completed} - description ${todoItem.description}
+      </li>`;
+  }
+  list.innerHTML = html;
+  h1.innerText = "Todo List";
 
-    app.appendChild(h1)
-    // and maybe some here
+  app.appendChild(h1);
+  document.body.appendChild(list);
 }
 
-renderTodoApp()
+renderTodoApp();
